e1c9ebfe46d57ba7a311514c092632e46ce4a21c
Victor <mwendwav1789@gmail.com>
2024-09-24 20:29:50 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution in this file!
+let companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+const companyNameTest = () {
+    describe('companyName', function () {
+        it('is set as Scuber', function () {
+          expect (companyName).to equal('Scuber')
+        });  
+    });    
+};  
